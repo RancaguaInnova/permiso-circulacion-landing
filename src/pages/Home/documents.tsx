998e9a23770f46755e229b08393f49fa4a817bd2
José Abarca Saavedra <jose@smart.rancagua.cl>
2020-02-13 17:00:42 -0300

    document information

--- src/pages/Home/documents.tsx
+++ src/pages/Home/documents.tsx
@@ -11,9 +11,12 @@ const Documents: React.FC = () => {
                 <div className='col-md-12 location-box'>
                     <h3>EDIFICIO TOMAS GUAGLÉN</h3>
                     <p>1. Permiso de Circulación pago total año anterior.</p>
-                    <p>2. Permiso de Circulación pago total año anterior.</p>
-                    <p>3. Permiso de Circulación pago total año anterior.</p>
-                    <p>4. Permiso de Circulación pago total año anterior.</p>
+                    <p>2. Seguro Obligatorio de Accidentes Personales (SOAP).</p>
+                    <p>3. Copia Revisión Técnica y Análisis de Gases o copia Homologación Vigente.</p>
+                    <p>
+                        4. Cambio de propietario o modificación alguna de características del vehiculo, deberá presentar
+                        Certificado de Dominio (emitido por el registro civil).
+                    </p>
                 </div>
             </div>
         </section>
