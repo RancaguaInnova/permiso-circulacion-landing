0ee3ab0add1d804f7902e267dfadca32a0eee201
José Abarca Saavedra <jose@smart.rancagua.cl>
2020-02-17 15:26:41 -0300

    update landing text + manifest

--- src/components/RenewButton/index.tsx
+++ src/components/RenewButton/index.tsx
@@ -6,9 +6,12 @@ const RenewButton: React.FC = () => {
         <div className='row text-left'>
             <div className='col-md-6'>
                 <div className='button-box p-4'>
-                    <button className='btn btn-danger btn-lg btn-block btn-renew'>
-                        RENUEVA TU PERMISO <br />DE CIRCULACION AQUÍ
-                    </button>
+                    <a
+                        href='https://pago.smc.cl/pagoPCVv2/Login.aspx?ReturnUrl=%2fpagopcvv2%2fSistema%2fPrincipal.aspx'
+                        className='btn btn-danger btn-lg btn-block btn-renew'
+                    >
+                        RENUEVA TU PERMISO <br />DE CIRCULACIÓN AQUÍ
+                    </a>
                     <a href='https://pago.smc.cl/pagoPCVv2/Login.aspx?ReturnUrl=%2fpagopcvv2%2fSistema%2fPrincipal.aspx'>
                         <img src='/assets/images/mouse_hand.png' className='btn-image' />
                     </a>
--- src/pages/Home/documents.tsx
+++ src/pages/Home/documents.tsx
@@ -9,7 +9,7 @@ const Documents: React.FC = () => {
             </div>
             <div className='row mt-4'>
                 <div className='col-md-12 location-box'>
-                    <h3>EDIFICIO TOMAS GUAGLÉN</h3>
+                    {/* <h3>EDIFICIO TOMAS GUAGLÉN</h3> */}
                     <p>1. Permiso de Circulación pago total año anterior.</p>
                     <p>2. Seguro Obligatorio de Accidentes Personales (SOAP).</p>
                     <p>3. Copia Revisión Técnica y Análisis de Gases o copia Homologación Vigente.</p>
