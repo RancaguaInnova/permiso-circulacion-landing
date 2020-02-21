0985e7b01af6154ba3af3761122bb085a60639ff
José Abarca Saavedra <jose@smart.rancagua.cl>
2020-02-21 08:48:55 -0300

    add CDIR signature

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,6 +5,9 @@ const Footer: React.FC = () => {
     return (
         <div className='Footer'>
             <img className='img-footer' alt='' src='/assets/images/footer.png' />
+            <p className='text-right'>
+                Desarrollado por <a href='mailto:contacto@smartrancagua.cl'>Corporación de Desarrollo e Innovación</a>
+            </p>
         </div>
     )
 }
