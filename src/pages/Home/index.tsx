7066049ac32ebb421bbbdc3b671804c0e0a264d4
jeperezr <jaime.e.perez.r@hotmail.com>
2020-04-09 17:32:15 -0400

    se quita horarios y se agrega msj de pago online

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,8 +9,12 @@ import Documents from './documents'
 const Home: React.FC = () => {
     return (
         <div className='container Home'>
-            <Hours />
-            <Documents />
+            <div className="alert alert-warning" role="alert">
+            <h5>Atención : Debido a la contingencia del país, el trámite sólo se realizará vía pago online.</h5>
+</div>
+
+           
+           <Documents />
             <RenewButton />
             {isMobile && <img className='img-fluid' alt='' src='/assets/images/pay_info.png' />}
         </div>
